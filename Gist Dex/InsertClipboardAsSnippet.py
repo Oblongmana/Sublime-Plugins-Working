667f86a6b541de916d693c0b3212cf9c815415a6
James Hill <oblongmana@gmail.com>
2013-05-02 12:23:24 +1200

    Initial commit: including Gist Dex plugin with key/cmd reg

---
+++ Gist Dex/InsertClipboardAsSnippet.py
@@ -0,0 +1,7 @@
+import sublime, sublime_plugin
+
+class InsertClipboardAsSnippetCommand(sublime_plugin.TextCommand):
+    def run(self,edit):
+        self.view.run_command('insert_snippet', {'contents': sublime.get_clipboard()})
+
+    
